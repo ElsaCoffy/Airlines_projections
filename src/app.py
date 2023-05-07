40b2112af0710274110c4f43188d4bb97664d353
Elsa COFFY <elsacoffy@gmail.com>
2023-05-07 11:38:18 +0200

    Interfacing the prediction

--- src/app.py
+++ src/app.py
@@ -31,7 +31,6 @@ st.write('Paired Airport selected:', paired_airport)
 st.write('Days of forecast:', nb_days)
 st.write('Date selected:', forecast_date)
 
-st.write(type(forecast_date))
 if run_forecast: 
     ## Getting the historical traffic data 
     traffic_df  = generate_route_df(df,home_airport,paired_airport).drop(columns=["home_airport","paired_airport"])
@@ -41,20 +40,15 @@ if run_forecast:
     complete_data = forecast_data(df,home_airport,paired_airport,nb_days,forecast_date)
 
     complete_data=  complete_data.rename(columns={'ds': 'date', 'yhat': 'prediction'})
-    st.text(traffic_df.dtypes)
-    st.text(complete_data.dtypes)
+
     complete_data.merge(traffic_df, on ='date',sort=True)
 
-    print(complete_data.head())
-    print(complete_data.tail())
 
 
     st.markdown('# Table des vols, pour la destination choisie, avec le nombre de passager total par jour')
 
     st.markdown('## Table generated  from the forecast ')
-    st.dataframe(forecast_data, width=600, height=300)
+    st.dataframe(complete_data, width=600, height=300)
 
-    # 
-
-    st.plotly_chart(draw_ts_multiple(forecast_data, 'pax_total', covid_zone=True,display=False,prediction ='prediction'))
+    st.plotly_chart(draw_ts_multiple(complete_data, 'pax_total', covid_zone=True,display=False,prediction ='prediction'))
 
